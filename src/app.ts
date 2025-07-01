780a69aa4d813019b1ef90ffb7bc306f65c046cb
anurag1517 <157402526+anurag1517@users.noreply.github.com>
2025-07-01 16:43:26 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,28 @@
 // src/app.ts
-
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
-import learningPathRoutes from "./routes/learningPath";
-import cors from 'cors';
-import authRoutes from './routes/auth';
-import quizRoutes from "./routes/quiz";
-import quizSubmitRoutes from "./routes/quizSubmit";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth";
 import connectDB from "./config/db";
-import quizHistoryRoutes from "./routes/quizHistory";
-import recommendationRoutes from "./routes/recommendation";
-import exploreRoutes from "./routes/explore";
-import path from "path";
 
-//dotenv.config({ path: path.join(__dirname, "../../.env") });
 dotenv.config();
 
 const app = express();
 const PORT = 5000;
-
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",  // for local dev
-      "https://cps2-rust.vercel.app/api/login", // your deployed frontend
-    ],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin:[
+    "https://cps2-rust.vercel.app",
+    "http://localhost:5173", // for local development
+  ],
+  credentials: true,
+}));
 
 app.use(bodyParser.json());
-app.use("/api/explore", exploreRoutes);
-app.use("/api", learningPathRoutes);
-app.use('/api', authRoutes);
-//app.use("/api/quiz", quizRoutes);
-app.use("/api/quiz-history", quizHistoryRoutes);
-app.use("/api/recommendation", recommendationRoutes);
-//app.use("/api", quizSubmitRoutes);
-app.use("/api", quizRoutes);
-app.use("/api", quizSubmitRoutes);
 
-
-connectDB(); // before app.listen
+app.use("/api", authRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
+connectDB();
+export default app;
